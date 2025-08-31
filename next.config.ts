a5624af3929b34fd075e363c1fc9284b4d13aaa3
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-08-31 20:33:20 +0900

    Replaced rewrites with redirect

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/seoul/1",
-      },
-    ];
-  },
   output: "export",
 };
 
