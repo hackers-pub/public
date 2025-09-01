9be01d332618e25b3608829a7b56c9ad441aaad5
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-09-01 23:19:21 +0900

    Replaced invite link and moved corner

--- components/Seoul1.tsx
+++ components/Seoul1.tsx
@@ -4,23 +4,27 @@ export default function Seoul1() {
       <article className="w-full h-full max-w-[calc(100vh*210/297)] max-h-[calc(100vw*297/210)] aspect-[210/297] relative">
         <img
           src="/hackers-star.svg"
-          className="absolute w-1/12 right-[15%] top-[15%] rotate-[-30deg] animate-wiggle"
+          className="absolute w-1/12 right-[12%] top-[7%] rotate-[-30deg] animate-wiggle"
         />
         <img
           src="/hackers-star.svg"
-          className="absolute w-1/12 left-[18%] top-1/2 rotate-[30deg] animate-wiggle"
+          className="absolute w-1/12 right-[5%] top-[32%] rotate-[-30deg] animate-wiggle"
         />
         <img
           src="/hackers-star.svg"
-          className="absolute w-1/12 right-[16%] bottom-[38%] rotate-[135deg] animate-wiggle"
+          className="absolute w-1/12 left-[30%] top-1/2 rotate-[30deg] animate-wiggle"
         />
         <img
           src="/hackers-star.svg"
-          className="absolute w-1/12 left-[45%] bottom-[23%] rotate-[-135deg] animate-wiggle"
+          className="absolute w-1/12 right-[16%] bottom-[28%] rotate-[135deg] animate-wiggle"
         />
         <img
           src="/hackers-star.svg"
-          className="absolute w-1/12 left-[10%] bottom-[15%] rotate-[-45deg] animate-wiggle"
+          className="absolute w-1/12 left-[35%] bottom-[15%] rotate-[-135deg] animate-wiggle"
+        />
+        <img
+          src="/hackers-star.svg"
+          className="absolute w-1/12 left-[10%] bottom-[30%] rotate-[-45deg] animate-wiggle"
         />
         <img
           src="/hackers-star.svg"
@@ -34,7 +38,7 @@ export default function Seoul1() {
         />
         <img
           src="/hackers-ufo.svg"
-          className="w-1/2 absolute right-[5%] top-1/4 animate-anti-wiggle"
+          className="w-1/2 absolute right-[5%] top-1/3 animate-anti-wiggle"
         />
         <img
           src="/1st-meetup-seoul.svg"
@@ -42,32 +46,33 @@ export default function Seoul1() {
           alt="1st Meetup at Seoul"
         />
         <a
-          href="https://event-us.kr/hackerspubseoul/event/110961"
-          className="w-1/4 absolute right-[5%] bottom-[18%]"
+          href="https://hackers.pub/@kodingwarrior/invite/019904e6-0f62-77d4-98cc-61a7eee47f78"
+          className="w-1/4 absolute left-[5%] bottom-[5%]"
+          target="_blank"
         >
           <img
-            src="/1st-meatup-eventus-qrcode.svg"
+            src="/1st-meetup-invite-qrcode.svg"
             className="h-full w-full"
-            alt="EventUs QR Code"
+            alt="Invite QR Code"
           />
         </a>
         <a
           href="https://hackers.pub/@jakeseo"
-          className="h-[5%] absolute right-[5%] bottom-[12%]"
+          className="h-[8%] absolute right-[5%] bottom-[14.5%]"
         >
           <img
-            src="/1st-meetup-session1.svg"
+            src="/1st-meetup-session1-3lines.svg"
             className="h-full"
             alt="Code As A Canvas : 코드에서 예술작품이 되기까지 (@jakeseo@hackers.pub)"
           />
         </a>
-        <hr className="my-2 color-white w-[45%] absolute right-[5%] bottom-[10%]" />
+        <hr className="my-2 color-white w-[45%] absolute right-[5%] bottom-[13%]" />
         <a
           href="https://hackers.pub/@dalgona"
-          className="h-[5%] absolute right-[5%] bottom-[5%]"
+          className="h-[8%] absolute right-[5%] bottom-[5%]"
         >
           <img
-            src="/1st-meetup-session2.svg"
+            src="/1st-meetup-session2-3lines.svg"
             className="h-full"
             alt="폰트는 어떻게 만들어지는가 - NeoDGM 사례로 살펴보는 개발 후일담 (@dalgona@hackers.pub)"
           />
