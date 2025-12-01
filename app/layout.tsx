e8112d4dfd705bb787a285affcb74a8ef90bac96
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-12-01 17:01:38 +0900

    set metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://public.hackers.pub"),
   title: {
     default: "Hackers' Public",
     template: "Hackers' Public %s",
