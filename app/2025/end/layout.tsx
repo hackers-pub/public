48090aa5908c0ece8bb5fb7d4187e8f29d471c64
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-12-01 17:08:02 +0900

    set metadataBase

--- app/2025/end/layout.tsx
+++ app/2025/end/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://public.hackers.pub"),
   title: "2025 Retrospective & Lightning Talk",
   description:
     "Hackers' Public 2025 Retrospective & Lightning Talk - Join us for an exciting event!",
